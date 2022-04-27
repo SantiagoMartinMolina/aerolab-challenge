efabcef7f665cb0f42f1f1b32ec35859368da058
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-27 07:32:42 -0300

    Create a hook for get requests

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,53 +1,65 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 
-type UseFetchState<T> = {
+export type UseFetchState<T> = {
   state: "idle" | "loading" | "error" | "success";
   data: null | T;
   error: null | Error;
 };
 
-interface Props {
-  method?: "POST" | "GET";
-  url: string;
-  data?: {[key: string]: string | number};
-}
+const useFetch = <T>(key: string, url: string) => {
+  const initialState = (): UseFetchState<T> => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
 
-const useFetch = <T>({method = "GET", url, data}: Props) => {
-  const [fetchState, setFetchState] = useState<UseFetchState<T>>({
-    state: "idle",
-    data: null,
-    error: null,
-  });
+    if (valueInLocalStorage) {
+      return {
+        state: "success",
+        data: JSON.parse(valueInLocalStorage),
+        error: null,
+      };
+    } else {
+      return {
+        state: "idle",
+        data: null,
+        error: null,
+      };
+    }
+  };
+
+  const [fetchState, setFetchState] = useState<UseFetchState<T>>(initialState);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setFetchState((oldValue) => ({
-          ...oldValue,
-          state: "loading",
-        }));
+        if (fetchState.state === "idle") {
+          setFetchState((oldValue) => ({
+            ...oldValue,
+            state: "loading",
+          }));
 
-        const response = await axios({
-          method,
-          url,
-          headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjVmOTVmYWNjNmQ0ODAwMjEwOWEzY2EiLCJpYXQiOjE2NTA0MzE0ODJ9.c-cPjmGGRL9mb1wyoqzoDPcgnVba4kM1-wL9UDl-NzY`,
-          },
-        });
+          const response = await axios({
+            method: "GET",
+            url,
+            headers: {
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjVmOTVmYWNjNmQ0ODAwMjEwOWEzY2EiLCJpYXQiOjE2NTA0MzE0ODJ9.c-cPjmGGRL9mb1wyoqzoDPcgnVba4kM1-wL9UDl-NzY`,
+            },
+          });
 
-        if (response.status === 200) {
-          setFetchState({
-            data: response.data,
-            state: "success",
-            error: null,
-          });
-        } else {
-          setFetchState({
-            data: null,
-            state: "error",
-            error: new Error(response.statusText),
-          });
+          if (response.status === 200) {
+            window.localStorage.setItem(key, JSON.stringify(response.data));
+
+            setFetchState({
+              data: response.data,
+              state: "success",
+              error: null,
+            });
+          } else {
+            setFetchState({
+              data: null,
+              state: "error",
+              error: new Error(response.statusText),
+            });
+          }
         }
       } catch (error) {
         setFetchState({
@@ -59,9 +71,9 @@ const useFetch = <T>({method = "GET", url, data}: Props) => {
     };
 
     fetchData();
-  }, [url, method, data]);
+  }, [url, fetchState.state, key]);
 
-  return fetchState;
+  return {fetchState, setFetchState};
 };
 
 export default useFetch;
