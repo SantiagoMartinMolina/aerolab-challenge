853c4254de25b3a158914f3bbfc55164612e91ef
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-27 07:35:19 -0300

    Complete component using context

--- src/components/UserData/index.tsx
+++ src/components/UserData/index.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 
+import useUserContext from "~/hooks/useUserContext";
 import Coin from "../../assets/icons/Coin";
 
 import {StyledUserData} from "./styles";
 
 const UserData = () => {
+  const {
+    state: fetchState,
+    dispatch: {addPoints},
+  } = useUserContext();
+
+  if (fetchState.state === "loading" || fetchState.state === "idle") {
+    return <div>cargando...</div>;
+  }
+
+  if (fetchState.state === "error" || !fetchState.data) {
+    return <div>error</div>;
+  }
+
+  const handleButton = () => {
+    addPoints(1000).then((res) => console.log(res));
+  };
+
   return (
     <StyledUserData>
-      <p className="user-name">John Kite</p>
+      <p className="user-name">{fetchState.data.name}</p>
       <p className="user-points">
-        6000 <Coin />
+        {fetchState.data.points} <Coin />
       </p>
+      <button onClick={handleButton}>Agregar puntos</button>
     </StyledUserData>
   );
 };
